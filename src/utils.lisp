d4eb92faf1a7eae18127cbaa64a2e5f77e3c9f73
Gleefre <varedif.a.s@gmail.com>
2023-07-15 18:18:27 +0200

    Add data-path

--- src/utils.lisp
+++ src/utils.lisp
@@ -1,1 +1,14 @@
 (in-package #:low-battery)
+
+;; data-path to get resource's path
+(defparameter *data-location* "res/")
+
+(let ((data-folder nil))
+  (defun data-path (relative-path)
+    (setf data-folder
+          (or data-folder
+              (if (member :deploy *features*)
+                  (let ((deploy:*data-location* *data-location*))
+                    (deploy:data-directory))
+                  (asdf:system-relative-pathname "low-battery" *data-location*))))
+    (format nil "~a" (merge-pathnames relative-path data-folder))))
