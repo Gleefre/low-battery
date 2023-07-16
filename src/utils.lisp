705a4c1c33b4d88f477d9b6d11fa3edf1eb3c535
Gleefre <varedif.a.s@gmail.com>
2023-07-16 16:49:58 +0200

    Add with-split macro (maybe future "box layout")

--- src/utils.lisp
+++ src/utils.lisp
@@ -15,3 +15,26 @@
 
 (defun pic (name)
   (uiop:native-namestring (merge-pathnames name (data-path "icons/"))))
+
+(defmacro with-split ((width-var height-var &optional (orientation :horizontal))
+                      &body size-body)
+  (declare (type (member :horizontal :vertical) orientation))
+  (let (($sizes (loop for clause in size-body collect (gensym "size")))
+        ($sum-of-sizes (gensym "sum-of-sizes")))
+    `(let (,@(loop for (size . body) in size-body
+                   for $size in $sizes
+                   collect `(,$size ,size)))
+       (let ((,$sum-of-sizes (+ ,@$sizes)))
+         (s:with-current-matrix
+           ,@(loop for (size . body) in size-body
+                   for $size in $sizes
+                   collect `(let (,@(if (eq orientation :horizontal)
+                                        `((,height-var ,height-var)
+                                          (,width-var (* (/ ,$size ,$sum-of-sizes) ,width-var)))
+                                        `((,width-var ,width-var)
+                                          (,height-var (* (/ ,$size ,$sum-of-sizes) ,height-var)))))
+                              ,@body)
+                   collect (if (eq orientation :horizontal)
+                               `(s:translate (* (/ ,$size ,$sum-of-sizes) ,width-var) 0)
+                               `(s:translate 0 (* (/ ,$size ,$sum-of-sizes) ,height-var))))))
+       nil)))
